cfb3685f28673375d1d938bd1d3ae2df91e344de
Jorge Vargas <jorge.vargas@gmail.com>
2013-11-27 22:06:40 -0400

    - Yay it works, now to parse the crappy HTML

--- consulta-dgii-python/rncquery.py
+++ consulta-dgii-python/rncquery.py
@@ -4,21 +4,21 @@ url = "http://www.dgii.gov.do/app/WebApps/Consultas/rnc/RncWeb.aspx"
 
 # NO F*ing clue what this is ask Ahmed
 data = {
-#          '__EVENTTARGET': '',
-#          '__EVENTARGUMENT': '',
-#          '__LASTFOCUS': '',
-#          '__VIEWSTATE': '/wEPDwUKMTY4ODczNzk2OA9kFgICAQ9kFgQCAQ8QZGQWAWZkAg0PDxYCHgdWaXNpYmxlZ2QWBAIBDw8WAh4EVGV4dAUeTm8gZXhpc3RlbiByZWdpc3Ryb3MgQXNvY2lhZG9zZGQCAw88KwALAGRkNbZ1Awnu5Ci/S4J0pkzl4lgkQrg=',
-#          '__EVENTVALIDATION': '/wEWBgKi/dSQAwKT04WJBAKM04WJBAKDvK/nCAKjwtmSBALGtP74CmGDH9QVaQPr1itVc3r/K/qvjJBc',
+          '__EVENTTARGET': '',
+          '__EVENTARGUMENT': '',
+          '__LASTFOCUS': '',
+          '__VIEWSTATE': '/wEPDwUKMTY4ODczNzk2OA9kFgICAQ9kFgQCAQ8QZGQWAWZkAg0PZBYCAgMPPCsACwBkZHTpAYYQQIXs/JET7TFTjBqu3SYU',
+          '__EVENTVALIDATION': '/wEWBgKl57TuAgKT04WJBAKM04WJBAKDvK/nCAKjwtmSBALGtP74CtBj1Z9nVylTy4C9Okzc2CBMDFcB',
           'rbtnlTipoBusqueda': '0',
-          'txtRncCed': '13095709',
+          'txtRncCed': '130957096',
           'btnBuscaRncCed': 'Buscar'
       }
 
 headers = {'Content-type': 'application/x-www-form-urlencoded'}
 
 r = requests.post(url,data=data,headers=headers)
-print r.status_code
+#print r.status_code
 with open("a.html",'w') as a:
-    a.write(r.text.encode('utf8'))
+    a.write(r.text)
     import webbrowser
     webbrowser.open("a.html")
