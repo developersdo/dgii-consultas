3012e8b3e65cc1f15dd356fc19456060619f8f0c
Daniel Paniagua <dpaniagua@gmail.com>
2013-11-29 18:40:41 -0400

    Commit to merge to DEVDO repo Original Repo: https://bitbucket.org/danielgpm/consulta-dgii-python

---
+++ consulta-dgii-python/consulta_dgii/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+consulta_dgii
+-------------
+
+Main package for Consulta DGII
+"""
+
+__version__ = '0.1"
---
+++ consulta-dgii-python/consulta_dgii/tests/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+__init__
+---------
+
+Contains testing helpers.
+"""
